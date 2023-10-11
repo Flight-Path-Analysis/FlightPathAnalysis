93190047c14a6ceab734f099b39bdd4b28061b84
Andrerg01 <andrerg01@gmail.com>
2023-10-10 21:08:08 -0500

    What I could do on gather_data script

--- scripts/gather_data.py
+++ scripts/gather_data.py
@@ -1,20 +1,20 @@
+# Path from this script to the repo's root directory
+ROOT_PATH = '.'
+import sys
+sys.path.append(ROOT_PATH)
+
+from src.backend import opensky_query
+from src.backend import utils
+from src.backend import compressors
+
 import matplotlib.pyplot as plt
 import datetime
 import yaml
 import pandas as pd
 import numpy as np
 
-# Path from this script to the repo's root directory
-root_path = '.'
-import sys
-sys.path.append(root_path)
-
-from src.backend import opensky_query
-from src.backend import utils
-from src.backend import compressors
-
 # Loading config file
-with open(f'{root_path}/config/config.yml', 'r') as file:
+with open(f'{ROOT_PATH}/config/config.yml', 'r') as file:
     try:
         config = yaml.safe_load(file)
     except yaml.YAMLError as exc:
@@ -25,7 +25,7 @@ credentials_file = config['base-configs']['opensky-credentials']
 if not credentials_file:
     raise ValueError('No OpenSky credentials file specified in in config.yaml')
 
-with open(f'{root_path}/{credentials_file}', 'r') as file:
+with open(f'{ROOT_PATH}/{credentials_file}', 'r') as file:
     try:
         credentials = yaml.safe_load(file)
     except yaml.YAMLError as exc:
@@ -58,7 +58,7 @@ for airport_route in config['data-gather']['flights']['routes-of-interest']:
           destination_airport,
           start_date,
           end_date)
-    
+
     for i, flight in flights.iterrows():
         icao24 = flight['icao24']
         firstseen = flight['firstseen']
@@ -67,7 +67,7 @@ for airport_route in config['data-gather']['flights']['routes-of-interest']:
         estarrivalairport = flight['estarrivalairport']
         flight_id = f"{icao24}_{firstseen}_{lastseen}_\
         {estdepartureairport}_{estarrivalairport}"
-        
+
         state_vectors = opensky_querier.query_state_vectors(
                         icao24,
                         firstseen,
@@ -82,24 +82,25 @@ for airport_route in config['data-gather']['flights']['routes-of-interest']:
                          'geoaltitude',
                          'hour']
         for col in cols_to_check:
-            state_vectors[col] = state_vectors[col].apply(lambda x: np.nan if isinstance(x, str) else x)
+            state_vectors[col] = state_vectors[col].apply(
+                                 lambda x: np.nan if isinstance(x, str) else x)
         state_vectors.dropna(inplace=True)
 
         cols_to_check = ['lat', 'lon']
         state_vectors = state_vectors.drop_duplicates(subset=cols_to_check, keep='first')
-        
+
         # Encoding data
         metadata = compressor.encode_from_dataframe(state_vectors, 'time', columns)
         # Setting flight_id in the metadata
         metadata['flight_id'] = flight_id
         # Computes the compression factor achieved as x_old/x_new.
-        compression_factor = compressor.compute_compression_factor(state_vectors['time'].values, state_vectors[columns].values, metadata)
+        compression_factor = compressor.compute_compression_factor(
+                             state_vectors['time'].values, 
+                             state_vectors[columns].values, 
+                             metadata)
         print(f"Compression Factor: {compression_factor}")
         # Turns dictionary data into the yaml format
         yaml_data = yaml.dump(metadata, default_flow_style=None)
         # Saves it to a yaml file
-        with open(f"{root_path}/{config['data-gathe']['flights']}/{flight_id}.yml", 'w') as f:
+        with open(f"{ROOT_PATH}/{config['data-gathe']['flights']}/{flight_id}.yml", 'w') as f:
             f.write(yaml_data)
-        
-
-    
